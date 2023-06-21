527f1399d3a53fb9b1719c67dc895eea653a4c33
AlexandruIoan20 <117534646+AlexandruIoan20@users.noreply.github.com>
2023-06-21 11:18:59 +0300

    Providers not working - 1:43:34

--- promptopia/app/api/auth/[...nextauth]/route.js
+++ promptopia/app/api/auth/[...nextauth]/route.js
@@ -1,6 +1,8 @@
 import NextAuth from "next-auth/next";
-import GoogleProvider from "next-auth/providers/google"; 
+import GoogleProvider from "next-auth/providers/google";
+
 import { connectToDB } from "@utils/database";
+import User from "@models/user";
 
 const handler = NextAuth({ 
     providers: [ 
@@ -11,7 +13,12 @@ const handler = NextAuth({
     ], 
 
     async session ({ session }) { 
+        const sessionUser = await User.findOne({ 
+            email: session.user.email
+        }); 
 
+        session.user.id = sessionUser._id.toString(); 
+        return session; 
     }, 
 
     async signIn({ profile }) { 
@@ -19,8 +26,18 @@ const handler = NextAuth({
             await connectToDB(); 
 
             //check if a user already exists
+            const userExists = await User.findOne({ 
+                email: profile.email, 
+            }) ; 
 
             // if not, create a new user 
+            if(!userExists) { 
+                await User.create({ 
+                    email: profile.email, 
+                    username: profile.name.replace(" ", "").toLowerCase(), 
+                    image: profile.picture,
+                })
+            }
 
             return true;
         } catch (error) { 
